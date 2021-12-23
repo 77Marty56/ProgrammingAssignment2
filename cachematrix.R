3671eddef943c5d175f620a73f3a593dc63b5521
77Marty56 <96536243+77Marty56@users.noreply.github.com>
2021-12-23 19:56:42 +0100

    Update cachematrix.R
    Assignment 2 R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2, R programming, Coursera, peer reviewed
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates a special matrix object
+makeCacheMatrix <- function(X = matrix()){
+  
+  I <- NULL
+  
+  ## matrix passed by user as input is set
+  set <- function(Y){
+    X <<- Y
+    I <<- NULL
+  }
+  
+  ## get matrix out
+  get <- function(){X}
+  
+  ## inverse of matrix passed by user as input is set
+  setinverse <- function(inverse){I <<- inverse}
+  
+  ## get inverse of matrix out
+  getinverse <- function(){I}
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Computes the inverse of the special matrix object. If the inverse has already been calculated
+## and stored in the special matrix object, the function returns it. Otherwise, computes and returns
+## the inverse of the special matrix
+cacheSolve <- function(X, ...){
+  
+  ## get the inverse stored in special matrix object
+  ## could also be NULL
+  I <- X$getinverse()
+  
+  ## check if the inverse is not NULL and if matrix has not changed
+  if(!is.null(I)){
+    #we return the inverse, no need to compute it
+    message("Getting chached data")
+    return(I)
+  }
+  
+  ## if we hit this point, it means that the inverse was NULL
+  ## Get the matrix
+  data <- X$get()
+  
+  ## Compute the inverse using solve()
+  I <- solve(data, ...)
+  
+  ## Set the inverse in the special matrix object
+  X$setinverse(I)
+  
+  I
 }
